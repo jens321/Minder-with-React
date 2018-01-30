f5d91927428a7d05ef33df8f39d48ee347697a64
Jens Tuyls <you@example.com>
2018-01-29 16:40:33 -0800

    refactored backend

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,8 @@ let Chat = require('../models/chat.js');
 let auth = require('../utils/auth'); 
 let fs = require('fs'); 
 
+// ------------------ USER SIGNUP -------------------
+
 router.post('/api/signup', function(req, res, next) {
   let hashedPassword = User.hashPassword(req.body.password);
 
@@ -33,29 +35,38 @@ router.post('/api/signup', function(req, res, next) {
 
 }); 
 
+// ------------------- UPDATE USER --------------------
+
 router.patch('/api/user/:id', function(req, res, next) {
   let body = req.body;   
   if (body.image) { 
-    let path = `${__dirname}/../public/images/profile/${req.params.id}.png`; 
-    let buffer = new Buffer(body.image, 'base64');
-
-    if(fs.existsSync(path)) {
-      fs.unlinkSync(path); 
-    }
-
-    fs.writeFileSync(path, buffer); 
+    let path = saveProfileImage(body.image, req.params.id); 
+    
     // add meaningless query parameter in order for react to update the image
-    body.imageUrlPath = `/images/profile/${req.params.id}.png?key=${new Date()}`;
-    res.json(body);  
+    body.imageUrlPath = path; 
+    res.json(body); 
+  } else {
+    delete body.editable; 
+    User.findByIdAndUpdate(mongoose.Types.ObjectId(req.params.id), body, {new: true}, function(err, newUser) {
+      if (err) throw err; 
+      delete newUser.password; 
+      res.status(200).json(newUser); 
+    });
+  } 
+});
+
+// ---------------- HELPER FUNCTIONS ------------------
+
+let saveProfileImage = (image, id) => {
+  let path = `${__dirname}/../public/images/profile/${id}.png`; 
+  let buffer = new Buffer(image, 'base64');
+
+  if(fs.existsSync(path)) {
+    fs.unlinkSync(path); 
   }
 
-  // delete body.editable; 
-  // User.findByIdAndUpdate(mongoose.Types.ObjectId(req.params.id), body, {new: true}, function(err, newUser) {
-  //   if (err) throw err; 
-  //   delete newUser.password; 
-  //   res.status(200).json(newUser); 
-  // }); 
-
-});
+  fs.writeFileSync(path, buffer); 
+  return `/images/profile/${req.params.id}.png?key=${new Date()}`;; 
+}
 
 module.exports = router;
